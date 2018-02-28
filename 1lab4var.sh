f230ced751083ec5999d9f7b433e4609a68261fe
vladuken <vladpetrushkevich1999@gmail.com>
2018-02-28 20:08:03 +0300

    Hope its final version

--- 1lab4var.sh
+++ 1lab4var.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-(
 dir1=$1
 dir2=$2
+num=$(find $dir1 $dir2 -type f| wc -l)
+(
 
 
 #count number of equall files
@@ -10,12 +11,20 @@ checked=0
 
 IFS=$'\n'
 
-for var1 in $(find $dir1 -type f)
+set1=$(find $dir1 -type f)
+set2=$(find $dir2 -type f)
+
+#num=${set1}${set2}
+
+echo $num
+
+
+for var1 in $set1
 do
 	if [ -f "$var1" ]
 	then
 		echo $var1
-		for var2 in $(find $dir2 -type f)
+		for var2 in $set2
 		do
 			#if [ "$(stat -c$s "$var1")" == "$(stat -c$s "$var2")" ]
 			#then
@@ -40,8 +49,9 @@ done
 
 
 sed -i "s/^/$(basename $0:" ")/g"  /tmp/errors.txt 
-#cat /tmp/errors.txt
+cat /tmp/errors.txt >&2
 
-echo $(find $dir1 $dir2 -type f | wc -l)
+echo $num
+
  
 
