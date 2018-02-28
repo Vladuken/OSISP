287c763cdb6619b6f09435425f0f6e55f58b150e
vladuken <vladpetrushkevich1999@gmail.com>
2018-02-28 14:18:55 +0300

    Add spaces ignore in find command

--- 1lab4var.sh
+++ 1lab4var.sh
@@ -7,6 +7,9 @@ dir2=$2
 
 #count number of equall files
 checked=0
+
+IFS=$'\n'
+
 for var1 in $(find $dir1 -type f)
 do
 	if [ -f "$var1" ]
@@ -14,14 +17,20 @@ do
 		echo $var1
 		for var2 in $(find $dir2 -type f)
 		do
-			if [ -f "$var2" ]
-			then
-				if (diff -q $var1 $var2)
+			#if [ "$(stat -c$s "$var1")" == "$(stat -c$s "$var2")" ]
+			#then
+
+				if [ -f "$var2" ]
 				then
-					echo  $var1 = $var2 
-					let "checked=checked + 1"
-				fi
-			fi
+					if (diff -q $var1 $var2)
+					then
+						echo  $var1 = $var2 
+						let "checked=checked + 1"
+					fi
+				fi			
+	
+			#fi			
+						
 		done
 	fi
 done 
@@ -30,10 +39,9 @@ done
 ) 2>/tmp/errors.txt | grep =
 
 
-sed -i "s/^/$(basename $0:" ")/g"  /tmp/errors.txt
+sed -i "s/^/$(basename $0:" ")/g"  /tmp/errors.txt 
 #cat /tmp/errors.txt
 
 echo $(find $dir1 $dir2 -type f | wc -l)
  
-
 
